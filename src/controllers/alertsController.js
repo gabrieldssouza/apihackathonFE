cba36854ad91c85a2040950e8be7766a89e6121e
Pedro Haubert <pedrohaubert7@gmail.com>
2024-10-19 17:33:49 -0300

    Add new route for alerts by address

--- src/controllers/alertsController.js
+++ src/controllers/alertsController.js
@@ -33,6 +33,35 @@ exports.getAllAlerts = async (req, res) => {
     }
 };
 
+exports.getAlertsByAddress = async (req, res) => {
+    const { cidade, bairro } = req.query;
+    console.log(cidade, bairro);
+    let query = 'SELECT * FROM alerts';
+    const params = [];
+
+    if (cidade || bairro) {
+        query += ' WHERE';
+        if (cidade) {
+            query += ' cidade = $1';
+            params.push(cidade);
+        }
+        if (bairro) {
+            if (params.length > 0) {
+                query += ' AND';
+            }
+            query += ' bairro = $2';
+            params.push(bairro);
+        }
+    }
+
+    try {
+        const result = await pool.query(query, params);
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao listar alertas' });
+    }
+};
+
 exports.updateAlert = async (req, res) => {
     const { id } = req.params;
     const { tipo_alerta, mensagem, cidade, bairro, enviado_por } = req.body;
